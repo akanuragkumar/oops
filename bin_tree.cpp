de5c9fd0a2e5086ab665b16aed56c3b32efa90b3
Anurag Kumar <akanuragkumar712@gmail.com>
2016-11-22 09:19:28 +0530

    Update bin_tree.cpp

--- bin_tree.cpp
+++ bin_tree.cpp
@@ -1,113 +1,72 @@
-#include<iostream.h>
-#include<conio.h>
-#include<process.h>
-struct tree_node
-{
-	tree_node *left;
-	tree_node *right;
-	int data;
-} ;
-class bst
-{
-	tree_node *root;
-	public:
-	bst()
+                        #include<iostream>
+using namespace std;
+class node{public : int data; node * left ; node * right;};
+
+class BT{public :
+            node *root;
+            
+			 BT(){root=NULL;}
+			 void insert()
+			 { int d;
+			   cout<<"\nENter value : ";
+			   cin>>d;
+			   node *tmp=new node;
+			   node *parent,*current;
+			   tmp->data=d;
+			   tmp->left=tmp->right=NULL;
+			   if(root==NULL) root=tmp;
+			   else{ current=root;parent=NULL;
+			        while(1)
+			        {  parent=current;
+			           if(d<parent->data)
+			           {
+			              current=current->left;
+			              if(current==NULL)
+			              {parent->left=tmp;return;}
+			              }
+			              else
+			              { 
+			                 current=current->right;
+			                 if(current==NULL)
+			                 {parent->right=tmp;return;}}}}}
+			                 
+			                 
+			                 
+			       void pre(node * root)
+				  {  if(root!=NULL)
+				      {  cout<<root->data<<"\t";
+				          pre(root->left);
+				          pre(root->right);}}
+				          
+				          
+				    void post(node * root)
+				    {  if(root!=NULL)
+				       {
+				          post(root->left);
+				        post(root->right);
+				        cout<<root->data<<"\t";}}
+				        
+				    void in(node * root)
+				    {if(root!=NULL)
+				       {  
+				          in(root->left);
+				           cout<<root->data<<"\t";
+				           in(root->right);}}
+				       };
+				       
+				       
+	main()
 	{
-		root=NULL;
-	}
-	int isempty() 
-	{
-		return(root==NULL);
-	}
-	void insert(int item);
-	void inordertrav();
-	void inorder(tree_node *);
-	void postordertrav();
-	void postorder(tree_node *);
-	void preordertrav();
-	void preorder(tree_node *);
-};
-void bst::insert(int item)
-{
-	tree_node *p=new tree_node;
-	tree_node *parent;
-	p->data=item;
-	p->left=NULL;
-	p->right=NULL;
-	parent=NULL;
-	if(isempty())
-		root=p;
-	else
-	{
-		tree_node *ptr;
-		ptr=root;
-		while(ptr!=NULL)
-		{
-			parent=ptr;
-			if(item>ptr->data)		
-				ptr=ptr->right;
-			else
-				ptr=ptr->left;
-		}	
-		if(item<parent->data)
-			parent->left=p;
-		else
-			parent->right=p;
-	}
-}
-void bst::inordertrav()
-{
-	inorder(root);
-}
-void bst::inorder(tree_node *ptr)
-{
-	if(ptr!=NULL)
-	{
-		inorder(ptr->left);
-		cout<<"  "<<ptr->data<<"     ";
-		inorder(ptr->right);
-	}
-}
-void bst::postordertrav()
-{
-	postorder(root);
-}
-void bst::postorder(tree_node *ptr)
-{
-	if(ptr!=NULL)
-	{
-		postorder(ptr->left);
-		postorder(ptr->right);
-		cout<<"  "<<ptr->data<<"     ";
-	}
-}
-void bst::preordertrav()
-{
-	preorder(root);
-}
-void bst::preorder(tree_node *ptr)
-{
-	if(ptr!=NULL)
-	{
-		cout<<"  "<<ptr->data<<"     ";
-		preorder(ptr->left);
-		preorder(ptr->right);
-	}
-}
-void main()
-{
-	bst b;
-	b.insert(52);
-	b.insert(25);
-	b.insert(50);
-	b.insert(15);
-	b.insert(40);
-	b.insert(45);
-	b.insert(20); cout<<"inorder"<<endl;
-	b.inordertrav();
-	cout<<endl<<"postorder"<<endl;
-	b.postordertrav();
-	cout<<endl<<"preorder"<<endl;
-	b.preordertrav();
-	
-}
+    int i;BT m;
+   m.insert(); m.insert(); m.insert(); m.insert(); m.insert(); m.insert(); m.insert();
+
+         
+         
+          cout<<"\nPreorder traversal: ";
+   m.pre(m.root);
+
+   cout<<"\nInorder traversal: ";
+   m.in(m.root);
+
+   cout<<"\nPost order traversal: ";
+   m.post(m.root);       }

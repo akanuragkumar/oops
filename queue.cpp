13721e6a4b71e177b8acdc2eed905f98be5edbe7
Anurag Kumar <akanuragkumar712@gmail.com>
2016-11-22 09:24:50 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -39,3 +39,60 @@ cout<<q[i]<<endl;
 }
 template<class Q>
 void QUEUE<Q>::del()
+{
+Q item;
+if(f==-1)
+{
+cout<<"queue is empty"<<endl;
+return;
+}
+item =q[f];
+cout<<"deleted element is"<<'\n'<<item<<endl;
+if(f==r)
+f=r=-1;
+else
+f++;
+if(f==-1)
+{
+cout<<"queue is empty"<<endl;
+return;
+}
+cout<<"contents of the queue"<<endl;
+for(int i=f;i<=r;i++)
+cout<<q[i]<<endl;
+}
+int main()
+{
+QUEUE<int>x;
+QUEUE<double>y;
+int ch,i;
+double d;
+cout<<"queue operations"<<endl;
+for(;;)
+{
+cout<<"1.insert integer 2.delete integer"<<endl;
+cout<<"3.insert double 4.delete double "<<endl;
+cout<<"5.exit"<<endl; cout<<"enter ur option"<<endl;
+cin>>ch;
+switch(ch)
+{
+case 1 :
+cout<<"enter an integer"<<endl;
+cin>>i;
+x.insert(i);
+break;
+case 2 :
+x.del();
+break;
+case 3 :
+cout<<"enter a double"<<endl;
+cin>>d;
+y.insert(d);
+break;
+case 4 :
+y.del();
+break;
+}
+}
+}
+    
